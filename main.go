afa2e336c4fd119d5b9103b6cf4f59f19284c37c
Danielle Calil <153567808+DanielleCalil@users.noreply.github.com>
2025-03-19 11:08:08 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/DanielleCalil/goppotunities/router"
+import (
+	"github.com/DanielleCalil/goppotunities/config"
+	"github.com/DanielleCalil/goppotunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
